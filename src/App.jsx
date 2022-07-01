66d2679428bd67a89e9858853667857c2508c87a
Naoya Matsufuji <n.matsufuji06@gmail.com>
2022-07-01 12:45:16 +0900

    コーディング

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,41 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力"></input>
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了済みのTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>ううう</li>
+            <button>戻す</button>
+          </div>
+          <div className="list-row">
+            <li>えええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import ReactDom from "react-dom";
 import { App } from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
