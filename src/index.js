838b0f9a8474fcc0f5a91acfdd007605da843193
Naoya Matsufuji <n.matsufuji06@gmail.com>
2022-07-01 12:23:36 +0900

    雛形作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+ReactDom.render(<App />, document.getElementById("root"));
 
 root.render(
   <StrictMode>
